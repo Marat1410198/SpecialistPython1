7d76ee4fae87555ba48d68105ba32d4bb332dc7a
Marat1410198 <106874053+Marat1410198@users.noreply.github.com>
2022-06-13 22:02:23 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -2,6 +2,18 @@
 # элементами которого будут квадратные корни элементов исходного списка,
 # но только если результаты извлечения корня не имеют десятичной части и
 # если такой корень вообще можно извлечь
-# Пример:
-# Дано: [2, -5, 8, 9, -25, 25, 4]
-# Результат: [3, 5, 2]
+
+import random
+
+n = int(input("Количество элементов в списке:"))
+numbers = []
+numbers_root = []
+
+for i in range(n):
+    numbers.append(random.randint(-100, 100))
+print(numbers)
+
+for i in numbers:
+    if i >= 0 and i ** 0.5 % 1 == 0:
+       numbers_root.append(i)
+print(numbers_root)
