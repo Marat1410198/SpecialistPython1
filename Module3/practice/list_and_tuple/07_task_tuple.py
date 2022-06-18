af792116886a1ccda51afa68e6838ba5f7d7d4de
Marat1410198 <106874053+Marat1410198@users.noreply.github.com>
2022-06-18 13:58:22 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -1,5 +1,10 @@
 # Дан кортеж заполненный целыми числами
 # Найдите самый большой элемент кортежа
 tup = (2, 4, 6, -4, 12, 0, 5)
+total = 0
 
-# TODO: your code here
+for i in tup:
+    if total < i:
+        total = i
+
+print(total)
